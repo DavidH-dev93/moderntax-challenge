6318bd4b620955213439b9c1f6c353c092148672
gregmalcolm <greg@gregmalcolm.com>
2018-12-17 14:47:47 -0500

    HU-237: Indicate how an input file can be supplied in the command line

--- lib/boxing/kata/boxing_kata.rb
+++ lib/boxing/kata/boxing_kata.rb
@@ -4,9 +4,15 @@ module Boxing
   module Kata
 
     def self.report
-      puts "Boxing Kata output (optional)"
+      unless has_input_file?
+        puts "Usage: ./bin/boxing-kata << spec/fixtures/family_preferences.csv"
+      end
 
       # Starting point for your code...
+    end
+
+    def self.has_input_file?
+      ARGF.filename != "-"
     end
   end
 end

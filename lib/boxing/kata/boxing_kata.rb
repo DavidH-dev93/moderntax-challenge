80dcc95072d6cbe9d5701e603ec80960e05c17b2
gregmalcolm <greg@gregmalcolm.com>
2018-12-19 09:30:21 -0500

    Fixed instructions and stdin detection

--- lib/boxing/kata/boxing_kata.rb
+++ lib/boxing/kata/boxing_kata.rb
@@ -5,14 +5,14 @@ module Boxing
 
     def self.report
       unless has_input_file?
-        puts "Usage: ./bin/boxing-kata <<spec/fixtures/family_preferences.csv"
+        puts "Usage: ruby ./bin/boxing-kata <spec/fixtures/family_preferences.csv"
       end
 
       # Starting point for your code...
     end
 
     def self.has_input_file?
-      ARGF.filename != "-"
+      !STDIN.tty?
     end
   end
 end
